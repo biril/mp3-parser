0a48e6db2d98cd7b3bf0461153a56407cedaecaa
biril <alexlambiris@gmail.com>
2013-08-24 22:46:49 +0200

    Bump version patch number -> v0.1.13

--- mp3-parser.js
+++ mp3-parser.js
@@ -1,4 +1,4 @@
-//     mp3-parser v0.1.12
+//     mp3-parser v0.1.13
 
 //     https://github.com/biril/mp3-parser
 //     Licensed and freely distributed under the MIT License
@@ -872,7 +872,7 @@
     Object.defineProperties(mp3Parser, {
 
         // Get current version of mp3-parser
-        version: { get: function () { return "0.1.12"; } }
+        version: { get: function () { return "0.1.13"; } }
     });
 
     return mp3Parser;
