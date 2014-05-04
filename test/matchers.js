5cdbacaec8b1ed9a24eb4700612b17af0dcc1e7c
biril <alexlambiris@gmail.com>
2014-05-04 12:20:50 +0200

    Add custom Jasmine matchers

---
+++ test/matchers.js
@@ -0,0 +1,66 @@
+/*jshint node:true */
+
+"use strict";
+
+var _ = require("underscore"),
+
+    // Dump Array or Dataview
+    dumpCollection = function (collection, n) {
+        var dump = [],
+            i = 0,
+            l = _.isArray(collection) ? collection.length : collection.byteLength,
+            getCollectionValue = _.isArray(collection) ?
+                function (i) { return collection[i]; } :
+                function (i) { return collection.getUint8(i); };
+
+        if ((n || (n = 3)) >= l / 2) { n = l + 1; }
+
+        while (true) {
+            if (i === l) { break; }
+
+            dump.push(getCollectionValue(i++));
+
+            if (i === n) {
+                dump.push("...");
+                i = l - n;
+            }
+        }
+
+        return l + ":[" + dump.join(", ") + "]";
+    };
+
+module.exports = {
+
+    // Expect actual to be a DataView with given elements. `expected` can be a DataView or Array
+    asDataViewToEqual: function (expected) {
+        var getExpectedValue = _.isArray(expected) ?
+                function (i) { return expected[i]; } :
+                function (i) { return expected.getUint8(i); },
+            getExpectedLength = _.isArray(expected) ?
+                function (i) { return expected.length; } :
+                function (i) { return expected.byteLength; },
+            i;
+
+        if (this.actual.byteLength !== getExpectedLength()) {
+            this.message = function () {
+                return "Expected " + dumpCollection(this.actual) + " to be a DataView of length " +
+                    getExpectedLength();
+            };
+            return false;
+        }
+
+        for (i = 0; i < this.actual.byteLength; ++i) {
+            if (this.actual.getUint8(i) !== getExpectedValue(i)) {
+                this.message = function () {
+                    return "Expected " + dumpCollection(this.actual) +
+                        " to be a DataView with elements " + dumpCollection(expected) +
+                        ". (Expected " + getExpectedValue(i) + " at index " + i + ", got " +
+                        this.actual.getUint8(i) + ")";
+                };
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
