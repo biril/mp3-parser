78b6cbed01e2ff297b87ca3047993be1ed501d0b
biril <alexlambiris@gmail.com>
2014-05-30 12:41:16 +0200

    Improve error checking in browser example

--- examples/browser/app/mp3-loader.js
+++ examples/browser/app/mp3-loader.js
@@ -4,6 +4,12 @@
 define(["underscore", "jquery"], function (_, $) {
     "use strict";
 
+    var buildErrorMsg = function (status, statusText) {
+            var msg = "Status: " + status;
+            if (statusText) { msg += ", " + statusText; }
+            return msg;
+        };
+
     // Return the mp3-loader module
     return {
 
@@ -16,10 +22,14 @@ define(["underscore", "jquery"], function (_, $) {
             request.responseType = "arraybuffer";
             request.open("GET", url, true);
 
+            request.onerror = function (event) {
+                onLoaded(buildErrorMsg(request.status, request.statusText));
+            };
+
             request.onload = function (event) {
 
                 if (request.readyState !== 4 || request.status !== 200) {
-                    return onLoaded(request.status + ", " + request.statusText);
+                    return onLoaded(buildErrorMsg(request.status, request.statusText));
                 }
 
                 onLoaded(null, new DataView(request.response));
