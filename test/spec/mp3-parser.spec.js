bca831d2794cde0a921716e3880d9e954eb5a56d
biril <alexlambiris@gmail.com>
2016-11-05 20:16:33 +0000

    Improve mp3 parser spec

--- test/spec/mp3-parser.spec.js
+++ test/spec/mp3-parser.spec.js
@@ -4,18 +4,28 @@
 //     Licensed and freely distributed under the MIT License
 //     Copyright (c) 2013-2016 Alex Lambiris
 
-/*jshint node:true */
-/*global describe, it, expect  */
+/* jshint node:true, esversion:6 */
+/* global describe, it, expect  */
 "use strict";
 
-describe("mp3 parser", function () {
+const _ = require("underscore");
+
+const methodNames = [
+    "readFrameHeader",
+    "readFrame",
+    "readLastFrame",
+    "readId3v2Tag",
+    "readXingTag",
+    "readTags"
+];
+
+describe("mp3 parser", () => {
 
     var mp3Parser = require(__dirname + "/../../main.js");
 
-    // beforeEach(function () { });
-
-    it("should be exported", function() {
-        expect(mp3Parser).toBeDefined();
+    it(`should be exported, with expected API ${methodNames.join(", ")}`, () => {
+        expect(mp3Parser).toBeObject();
+        _.each(methodNames, methodName => expect(mp3Parser).toHaveMethod(methodName));
     });
 
 });
