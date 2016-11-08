e045285c4ca873ce2e902a145de9b7ab31dfb946
biril <alexlambiris@gmail.com>
2016-11-08 22:02:25 +0000

    Convert CHAP frame spec to ES6

--- test/spec/read-id3v2.3-chap-iso-8859-1.spec.js
+++ test/spec/read-id3v2.3-chap-iso-8859-1.spec.js
@@ -10,30 +10,27 @@
 /* global jasmine, describe, beforeEach, it, expect */
 "use strict";
 
-var _ = require("underscore");
-var util = require("../util");
-var matchers = require("../matchers.js");
+const _ = require("underscore");
+const util = require("../util");
+const matchers = require("../matchers.js");
 
-var mp3Parser = require("../../main.js");
+const mp3Parser = require("../../main.js");
 
-var mp3FilePath = __dirname + "/../data/id3v2.3-chap-iso-8859-1.mp3";
+const mp3FilePath = `${__dirname}/../data/id3v2.3-chap-iso-8859-1.mp3`;
 
-describe("ID3v2.3 reader run on CHAP frames with with ISO-8859-1 encoded subframes", function () {
+describe("ID3v2.3 reader run on CHAP frames with with ISO-8859-1 encoded subframes", () => {
     // Read the ID3v2 tag. This is done once, here, and all tests run on `capturedId3v2Tag`
-    var mp3FileView = util.dataViewFromFilePath(mp3FilePath);
-    var capturedId3v2Tag = mp3Parser.readId3v2Tag(mp3FileView);
+    const mp3FileView = util.dataViewFromFilePath(mp3FilePath);
+    const capturedId3v2Tag = mp3Parser.readId3v2Tag(mp3FileView);
 
     // Helper to get (an array of) all captured ID3v2 tag frames of given `id`
-    var getCapturedFrames = function (id) {
-        return _(capturedId3v2Tag.frames).filter(function (frame) {
-            return frame.header.id === id;
-        });
-    };
+    const getCapturedFrames = id =>
+        _(capturedId3v2Tag.frames).filter(frame => frame.header.id === id);
 
     // Expect that `numOfFrames` of `id` were captured. Returns an array of relevant
     //  captured frames - unless the `numOfFrames` expectation fails
-    var expectCapturedFrames = function (id, numOfFrames) {
-        var capturedFrames = getCapturedFrames(id);
+    const expectCapturedFrames = (id, numOfFrames) => {
+        const capturedFrames = getCapturedFrames(id);
         expect(capturedFrames.length).toBe(numOfFrames);
         return capturedFrames;
     };
@@ -41,7 +38,7 @@ describe("ID3v2.3 reader run on CHAP frames with with ISO-8859-1 encoded subfram
     // [ID3v2 CHAP frames](http://id3.org/id3v2.3.0#Declared_ID3v2_frames) along with their
     //  'friendly names' as defined in the spec and, in certain cases, an `expected` hash which
     //  defines values to test against
-    var id3v2TagFrames = {
+    const id3v2TagFrames = {
         CHAP: {
             name: "Chapter",
             expected: {
@@ -88,25 +85,23 @@ describe("ID3v2.3 reader run on CHAP frames with with ISO-8859-1 encoded subfram
         }
     };
 
-    beforeEach(function () { jasmine.addMatchers(matchers); });
+    beforeEach(() => { jasmine.addMatchers(matchers); });
 
-    it("should read CHAP: Chapters frame", function () {
-        var capturedFrames = expectCapturedFrames("CHAP", 2);
-
-        // Get expected and actual chapter frames, for the case of an empty chapter frame
-        var expectedEmptyChapter = id3v2TagFrames.CHAP.expected.withoutFrames;
-        var framesWithoutNestedFrames = _.filter(capturedFrames, function (frame) {
-            return frame.content.id === expectedEmptyChapter.id;
-        });
+    it("should read CHAP: Chapters frame", () => {
+        const capturedFrames = expectCapturedFrames("CHAP", 2);
 
         // Get expected and actual chapter frames, for the case of an empty chapter frame
-        var expectedChapterWithFrames = id3v2TagFrames.CHAP.expected.withFrames;
-        var framesWithNestedFrames = _.filter(capturedFrames, function (frame) {
-            return frame.content.id === expectedChapterWithFrames.id;
-        });
+        const expectedEmptyChapter = id3v2TagFrames.CHAP.expected.withoutFrames;
+        const framesWithoutNestedFrames =
+            _(capturedFrames).filter(frame => frame.content.id === expectedEmptyChapter.id);
+
+        // Get expected and actual chapter frames, for the case of an empty chapter frame
+        const expectedChapterWithFrames = id3v2TagFrames.CHAP.expected.withFrames;
+        const framesWithNestedFrames =
+            _(capturedFrames).filter(frame => frame.content.id === expectedChapterWithFrames.id);
 
         expect(framesWithoutNestedFrames.length).toBe(1);
-        var emptyChapter = framesWithoutNestedFrames[0];
+        const emptyChapter = framesWithoutNestedFrames[0];
         expect(emptyChapter.content.startTime).toBe(expectedEmptyChapter.startTime);
         expect(emptyChapter.content.endTime).toBe(expectedEmptyChapter.endTime);
         expect(emptyChapter.content.startOffset).toBe(expectedEmptyChapter.startOffset);
@@ -114,24 +109,23 @@ describe("ID3v2.3 reader run on CHAP frames with with ISO-8859-1 encoded subfram
         expect(emptyChapter.content.frames.length).toBe(expectedEmptyChapter.frames.length);
 
         expect(framesWithNestedFrames.length).toBe(1);
-        var chapterWithFrames = framesWithNestedFrames[0];
+        const chapterWithFrames = framesWithNestedFrames[0];
         expect(chapterWithFrames.content.startTime).toBe(expectedChapterWithFrames.startTime);
         expect(chapterWithFrames.content.endTime).toBe(expectedChapterWithFrames.endTime);
         expect(chapterWithFrames.content.startOffset).toBe(expectedChapterWithFrames.startOffset);
         expect(chapterWithFrames.content.endOffset).toBe(expectedChapterWithFrames.endOffset);
-        var chapterSubFrames = chapterWithFrames.content.frames;
+
+        const chapterSubFrames = chapterWithFrames.content.frames;
         expect(chapterSubFrames.length).toBe(expectedChapterWithFrames.frames.length);
-        var titleFrames = chapterSubFrames.filter(function (frame) {
-            return frame.header.id === "TIT2";
-        });
+
+        const titleFrames = _(chapterSubFrames).filter(frame => frame.header.id === "TIT2");
         expect(titleFrames.length).toBe(1);
-        var titleFrame = titleFrames[0];
+        const titleFrame = titleFrames[0];
         expect(titleFrame.content.name).toBe(expectedChapterWithFrames.frames[0].content.name);
-        var commentFrames = chapterWithFrames.content.frames.filter(function (frame) {
-            return frame.header.id === "COMM";
-        });
+
+        const commentFrames = _(chapterSubFrames).filter(frame => frame.header.id === "COMM");
         expect(commentFrames.length).toBe(1);
-        var commentFrame = commentFrames[0];
+        const commentFrame = commentFrames[0];
         expect(commentFrame.content.name).toBe(expectedChapterWithFrames.frames[1].content.name);
     });
 });
