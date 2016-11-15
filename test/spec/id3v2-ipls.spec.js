36da106b33b65a56f21fe6e315bdc7552cafe0dd
biril <alexlambiris@gmail.com>
2016-11-15 22:49:37 +0000

    Fix invalid-index bug in ID3v2 IPLS frame parsing
    Don't assume that final string in involved people list is null terminated.

--- test/spec/id3v2-ipls.spec.js
+++ test/spec/id3v2-ipls.spec.js
@@ -7,11 +7,11 @@
 // Read the content of an
 //  [involved people list frame](http://id3.org/id3v2.3.0#Involved_people_list). Contains
 //  names of those involved - those contributing to the audio file - and how they were
-//  involved. The body simply contains a terminated string with the involvement directly
-//  followed by a terminated string with the involvee followed by a new involvement and so
-//  on. In the current implementation however, the frame's content is parsed as a
-//  collection of strings without attaching special meaning.There may only be one "IPLS"
-//  frame in each tag
+//  involved. The body simply contains the first 'involvement' as a terminated string, directly
+//  followed by the first 'involvee' as a terminated string, followed by a second terminated
+//  involvement string and so on. However, in the current implementation the frame's content is
+//  parsed as a collection of strings without any semantics attached. There may only be one
+//  "IPLS" frame in each tag
 //
 // * Encoding:            a single octet where 0 = ISO-8859-1, 1 = UCS-2
 // * People list strings: a series of strings, e.g. string 00 (00) string 00 (00) ...
@@ -46,10 +46,39 @@ describe("ID3v2.3 parser, reading IPLS frame", () => {
         expect(frame.content.values[1]).toBe("second");
     });
 
+    it("should read IPLS frame with content: 0first0second", () => {
+        const frameView = util.buildFrameView({
+            id: "IPLS",
+            content: [0, "first", 0, "second"],
+            offset: frameOffset
+        });
+
+        const frame = parser.readId3v2TagFrame(frameView, frameOffset, frameView.byteLength);
+
+        expect(frame.content.encoding).toBe(0);
+        expect(frame.content.values[0]).toBe("first");
+        expect(frame.content.values[1]).toBe("second");
+    });
+
     it("should read IPLS frame with content: 0first0second0third0", () => {
         const frameView = util.buildFrameView({
             id: "IPLS",
             content: [0, "first", 0, "second", 0, "third", 0],
+            offset: frameOffset
+        });
+
+        const frame = parser.readId3v2TagFrame(frameView, frameOffset, frameView.byteLength);
+
+        expect(frame.content.encoding).toBe(0);
+        expect(frame.content.values[0]).toBe("first");
+        expect(frame.content.values[1]).toBe("second");
+        expect(frame.content.values[2]).toBe("third");
+    });
+
+    it("should read IPLS frame with content: 0first0second0third", () => {
+        const frameView = util.buildFrameView({
+            id: "IPLS",
+            content: [0, "first", 0, "second", 0, "third"],
             offset: frameOffset
         });
 
