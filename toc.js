f8da1e8b41014d20ac13777d6f9c369f36bc2cda
biril <alexlambiris@gmail.com>
2016-11-20 21:32:09 +0000

    Generate annotated source for version 0.3.0

--- toc.js
+++ toc.js
@@ -1,101 +1,5 @@
 window.files = [
 {
-  "path": "lib/id3v2.js.html",
-  "originalName": "id3v2.js",
-  "originalPath": "lib/id3v2.js",
-  "name": "id3v2.js.html",
-  "lang": "javascript",
-  "toc": [
-    {
-      "level": 3,
-      "slug": "read-an-id3v2-tag-frame",
-      "title": "Read an ID3v2 Tag Frame"
-    },
-    {
-      "level": 3,
-      "slug": "read-the-id3v2-tag",
-      "title": "Read the ID3v2 Tag"
-    }
-  ]
-},
-{
-  "path": "lib/lib.js.html",
-  "originalName": "lib.js",
-  "originalPath": "lib/lib.js",
-  "name": "lib.js.html",
-  "lang": "javascript",
-  "toc": [
-    {
-      "level": 3,
-      "slug": "read-a-frame-header",
-      "title": "Read a Frame Header"
-    },
-    {
-      "level": 3,
-      "slug": "read-a-frame",
-      "title": "Read a Frame"
-    }
-  ]
-},
-{
-  "path": "lib/xing.js.html",
-  "originalName": "xing.js",
-  "originalPath": "lib/xing.js",
-  "name": "xing.js.html",
-  "lang": "javascript",
-  "toc": [
-    {
-      "level": 3,
-      "slug": "read-the-xing-tag",
-      "title": "Read the Xing Tag"
-    }
-  ]
-},
-{
-  "path": "main.js.html",
-  "originalName": "main.js",
-  "originalPath": "main.js",
-  "name": "main.js.html",
-  "lang": "javascript",
-  "toc": [
-    {
-      "level": 3,
-      "slug": "tl-dr",
-      "title": "TL;DR"
-    },
-    {
-      "level": 3,
-      "slug": "read-a-frame-header",
-      "title": "Read a Frame Header"
-    },
-    {
-      "level": 3,
-      "slug": "read-a-frame",
-      "title": "Read a Frame"
-    },
-    {
-      "level": 3,
-      "slug": "read-the-last-frame",
-      "title": "Read the Last Frame"
-    },
-    {
-      "level": 3,
-      "slug": "read-the-id3v2-tag",
-      "title": "Read the ID3v2 Tag"
-    },
-    {
-      "level": 3,
-      "slug": "read-the-xing-tag",
-      "title": "Read the Xing Tag"
-    },
-    {
-      "level": 3,
-      "slug": "read-all-tags-up-to-first-frame",
-      "title": "Read all Tags up to First Frame"
-    }
-  ]
-},
-{
   "path": "index.html",
   "originalName": "README.md",
   "originalPath": "README.md",
@@ -154,8 +58,124 @@ window.files = [
     },
     {
       "level": 2,
+      "slug": "contributing",
+      "title": "Contributing"
+    },
+    {
+      "level": 2,
+      "slug": "changelog",
+      "title": "Changelog"
+    },
+    {
+      "level": 4,
+      "slug": "next-diff",
+      "title": "Next — Diff"
+    },
+    {
+      "level": 4,
+      "slug": "0-3-0-nov-20-2016-diff",
+      "title": "0.3.0 — Nov 20, 2016 — Diff"
+    },
+    {
+      "level": 2,
       "slug": "license",
       "title": "License"
+    }
+  ]
+},
+{
+  "path": "main.js.html",
+  "originalName": "main.js",
+  "originalPath": "main.js",
+  "name": "main.js.html",
+  "lang": "javascript",
+  "toc": [
+    {
+      "level": 3,
+      "slug": "tl-dr",
+      "title": "TL;DR"
+    },
+    {
+      "level": 3,
+      "slug": "read-a-frame-header",
+      "title": "Read a Frame Header"
+    },
+    {
+      "level": 3,
+      "slug": "read-a-frame",
+      "title": "Read a Frame"
+    },
+    {
+      "level": 3,
+      "slug": "read-the-last-frame",
+      "title": "Read the Last Frame"
+    },
+    {
+      "level": 3,
+      "slug": "read-the-id3v2-tag",
+      "title": "Read the ID3v2 Tag"
+    },
+    {
+      "level": 3,
+      "slug": "read-the-xing-tag",
+      "title": "Read the Xing Tag"
+    },
+    {
+      "level": 3,
+      "slug": "read-all-tags-up-to-first-frame",
+      "title": "Read all Tags up to First Frame"
+    }
+  ]
+},
+{
+  "path": "lib/id3v2.js.html",
+  "originalName": "id3v2.js",
+  "originalPath": "lib/id3v2.js",
+  "name": "id3v2.js.html",
+  "lang": "javascript",
+  "toc": [
+    {
+      "level": 3,
+      "slug": "read-an-id3v2-tag-frame",
+      "title": "Read an ID3v2 Tag Frame"
+    },
+    {
+      "level": 3,
+      "slug": "read-the-id3v2-tag",
+      "title": "Read the ID3v2 Tag"
+    }
+  ]
+},
+{
+  "path": "lib/lib.js.html",
+  "originalName": "lib.js",
+  "originalPath": "lib/lib.js",
+  "name": "lib.js.html",
+  "lang": "javascript",
+  "toc": [
+    {
+      "level": 3,
+      "slug": "read-a-frame-header",
+      "title": "Read a Frame Header"
+    },
+    {
+      "level": 3,
+      "slug": "read-a-frame",
+      "title": "Read a Frame"
+    }
+  ]
+},
+{
+  "path": "lib/xing.js.html",
+  "originalName": "xing.js",
+  "originalPath": "lib/xing.js",
+  "name": "xing.js.html",
+  "lang": "javascript",
+  "toc": [
+    {
+      "level": 3,
+      "slug": "read-the-xing-tag",
+      "title": "Read the Xing Tag"
     }
   ]
 }
